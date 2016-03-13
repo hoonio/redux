4cf3f96e9d114d6c5043c74edf103caf8275c57d
Hoonio <hoonio@hoonio.com>
2016-03-13 19:08:40 +0000

    replace provider with react-redux

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from  './app'
 import { combineReducers, createStore } from 'redux'
+import {Provider} from 'react-redux'
 
 const todo = (state, action) => {
   switch (action.type) {
@@ -195,20 +196,6 @@ const TodoApp = () => (
     <Footer />
   </div>
 );
-
-class Provider extends React.Component {
-  getChildContext(){
-    return {
-      store: this.props.store
-    };
-  }
-  render() {
-    return this.props.children;
-  }
-}
-Provider.childContextTypes = {
-  store: React.PropTypes.object
-};
 
 ReactDOM.render(
   <Provider store={createStore(todoApp)}>
