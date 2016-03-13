cfb5482fa5e868ee99655b46c40e2697db114ee0
Hoonio <hoonio@hoonio.com>
2016-03-13 02:19:04 +0000

    22 extract container component filterlink

--- main.js
+++ main.js
@@ -51,30 +51,53 @@ const todoApp = combineReducers({todos, visibilityFilter});
 
 const store = createStore(todoApp);
 
-const FilterLink = ({filter, currentFilter, children, onClick}) => {
-  if (filter === currentFilter) {
+const Link = ({ active, children, onClick }) => {
+  if (active) {
     return <span>{children}</span>
   }
   return (
     <a href='#' onClick={e => {
       e.preventDefault();
-      onClick(filter);
+      onClick();
     }} >{children}</a>
-  );
-};
+  )
+}
+
+class FilterLink extends React.Component {
+  componentDidMount() {
+    this.unsubscribe = store.subscribe(() =>
+      this.forceUpdate()
+    );
+  }
+  componentWillUnmount(){
+    this.unsubscribe();
+  }
+  render() {
+    const props = this.props;
+    const state = store.getState();
+    return (
+      <Link active={ props.filter === state.visibilityFilter }
+        onClick={() =>
+          store.dispatch({
+            type: 'SET_VISIBILITY_FILTER',
+            filter: props.filter
+          })
+        }
+      > {props.children} </Link>
+    );
+  }
+}
 
 const Footer = ({visibilityFilter, onFilterClick}) => (
   <p>
     Show:
-    {' '} <FilterLink filter='SHOW_ALL' currentFilter={visibilityFilter} onClick={onFilterClick}>All</FilterLink>
-    {' '} <FilterLink filter='SHOW_ACTIVE' currentFilter={visibilityFilter} onClick={onFilterClick}>Active</FilterLink>
-    {' '} <FilterLink filter='SHOW_COMPLETED' currentFilter={visibilityFilter} onClick={onFilterClick}>Completed</FilterLink>
+    {' '} <FilterLink filter='SHOW_ALL'>All</FilterLink>
+    {' '} <FilterLink filter='SHOW_ACTIVE'>Active</FilterLink>
+    {' '} <FilterLink filter='SHOW_COMPLETED'>Completed</FilterLink>
   </p>
 )
 
-const Todo = ({
-  onClick, completed, text
-}) => (
+const Todo = ({ onClick, completed, text }) => (
   <li onClick={onClick}
       style={{
         textDecoration: completed ? 'line-through':'none'
@@ -123,9 +146,7 @@ const getVisibleTodos = (todos, filter) => {
   }
 }
 
-
 let nextTodoId = 0;
-
 const TodoApp = ({ todos, visibilityFilter }) => (
   <div>
     <AddTodo onAddClick={text =>
